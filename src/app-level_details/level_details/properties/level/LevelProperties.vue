a516112273b3e2d27f3b67d1461280483429efe0
yezhiyi9670 <yezhiyi0562@foxmail.com>
2026-09-05 17:13:04 +0800

    Add level card display to level properties

--- src/app-level_details/level_details/properties/level/LevelProperties.vue
+++ src/app-level_details/level_details/properties/level/LevelProperties.vue
@@ -3,6 +3,7 @@ import { computed, inject } from 'vue';
 import type LevelItemData from '../../../../common/data_model/temples/LevelItemData.ts';
 import type LevelProgress from '../../../../common/data_model/progress/LevelProgress.ts';
 import TempleItemData from '../../../../common/data_model/temples/TempleItemData.ts';
+import LevelCardContent from '../../../components/LevelCardContent.vue';
 
 const props = defineProps<{
   level: LevelItemData
@@ -17,6 +18,12 @@ const json = computed(() => JSON.stringify(props.level, null, 2))
 
 <template>
   <div class="level-properties">
+    <div class="level-card">
+      <LevelCardContent
+        :level="level"
+        :progress="progress"
+      />
+    </div>
     <p class="props-placeholder">
       关卡属性（占位）· 圣殿 {{ temple?.label }} ({{ templeKey }})
       · 关卡 {{ props.level.getShownNumbering() }} · 进度：{{ props.progress ? '已存在' : '无' }}
@@ -26,6 +33,11 @@ const json = computed(() => JSON.stringify(props.level, null, 2))
 </template>
 
 <style lang="css" scoped>
+.level-card {
+  display: flex;
+  flex-direction: column-reverse;
+  gap: .5em;
+}
 .props-placeholder {
   margin: 0 0 8px;
   opacity: .7;
