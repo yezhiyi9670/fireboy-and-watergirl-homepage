cbc720d18ffdb5df3d238c84f17a25401b9c248d
yezhiyi9670 <yezhiyi0562@foxmail.com>
2026-09-06 22:37:43 +0800

    Remove storybook from screen

--- src/app-level_details/level_details/map/TemplesMapView.vue
+++ src/app-level_details/level_details/map/TemplesMapView.vue
@@ -3,7 +3,6 @@ import { computed, inject } from 'vue';
 import LsGameProgressData from '../../../common/data_model/progress/LsGameProgressData.ts';
 import MapTemple from './MapTemple.vue';
 import type ApiTemplesData from '../../../common/data_model/temples/ApiTemplesData.ts';
-import OmniInputStorybook from '../../../common/components/edit/OmniInputStorybook.vue';
 
 const props = defineProps<{
   data: ApiTemplesData
@@ -25,7 +24,6 @@ const progress = inject(LsGameProgressData.injectionKey)
       :temple-key="templeKey"
       :progress="progress?.getTempleById(temple.id)"
     />
-    <OmniInputStorybook />
   </div>
 </template>
 
